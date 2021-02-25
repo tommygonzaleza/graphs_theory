fc358f8c2d38036c635b9f0c382f72d9106c11fe
Jrios <Jrios@CONINFRA1-LGO>
2021-02-24 21:33:57 -0400

    clase Nodo lista

--- src/teoria_de_grafos/Nodo.java
+++ src/teoria_de_grafos/Nodo.java
@@ -10,5 +10,72 @@ package teoria_de_grafos;
  * @author Usuario
  */
 public class Nodo {
+    private Nodo pnext;
+    private String nombre;
+    private int anio;
+    private int ID;
+    
+    public Nodo(String nombre,int ID,int anio){
+        this.pnext=null;
+        this.nombre=nombre;
+        this.ID=ID;
+        this.anio=anio;
+    }
+
+    /**
+     * @return the pnext
+     */
+    public Nodo getPnext() {
+        return pnext;
+    }
+
+    /**
+     * @param pnext the pnext to set
+     */
+    public void setPnext(Nodo pnext) {
+        this.pnext = pnext;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the anio
+     */
+    public int getAnio() {
+        return anio;
+    }
+
+    /**
+     * @param anio the anio to set
+     */
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
     
 }
